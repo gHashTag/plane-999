775707f0ada148fb4e8e96dd2f143faff0921a3f
LAKHAN BAHETI <lakhanbaheti9@gmail.com>
2024-02-14 19:29:39 +0530

    style: workspace settings responsiveness

--- web/pages/[workspaceSlug]/settings/api-tokens.tsx
+++ web/pages/[workspaceSlug]/settings/api-tokens.tsx
@@ -60,7 +60,7 @@ const ApiTokensPage: NextPageWithLayout = observer(() => {
     <>
       <CreateApiTokenModal isOpen={isCreateTokenModalOpen} onClose={() => setIsCreateTokenModalOpen(false)} />
       {tokens ? (
-        <section className="h-full w-full overflow-y-auto py-8 pr-9">
+        <section className="h-full w-full overflow-y-auto md:py-8 py-3">
           {tokens.length > 0 ? (
             <>
               <div className="flex items-center justify-between border-b border-custom-border-200 py-3.5">
