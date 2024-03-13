3821e24f0d37f76c8786f067641ddf6f8b2e2162
Dmitriy Vasilev <raoffonom@icloud.com>
2024-03-13 19:40:19 +0700

    border yellow

--- web/components/999/LandingCards/SubCard.tsx
+++ web/components/999/LandingCards/SubCard.tsx
@@ -1,32 +1,31 @@
-import React from 'react'
-import Image from 'next/image'
-import LandingIcons from '@components/999/LandingCards/LandingIcons'
-
+import React from "react";
+import Image from "next/image";
+import LandingIcons from "@components/999/LandingCards/LandingIcons";
 
 type SubCardProps = {
-  title: string
-  img: string
-  onClick: () => void
-}
+  title: string;
+  img: string;
+  onClick: () => void;
+};
 
 const SubCard: React.FC<SubCardProps> = ({ title, img, onClick }) => {
   return (
     <a onClick={onClick}>
-      <div className="border rounded-lg border-custom-1 p-4 flex flex-col cursor-pointer hover:bg-[#fff]/50 transition duration-300 ease-in-out">
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '20px' }}>
+      <div className="p-4 flex flex-col cursor-pointer hover:bg-yellow-600 hover:bg-opacity-50 transition duration-300 ease-in-out">
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", marginBottom: "20px" }}>
           <Image
             src={`/images/${img}`}
-            alt={title ?? 'landing- card'}
+            alt={title ?? "landing- card"}
             width={400}
             height={400}
             className="object-contain"
-            style={{ marginBottom: '20px' }}
+            style={{ marginBottom: "20px" }}
           />
         </div>
         <div className="flex items-center justify-center">{LandingIcons[title]}</div>
         <div className="flex items-center justify-center text-slate-400 text-base font-medium mt-2">{title}</div>
       </div>
     </a>
-  )
-}
-export default React.memo(SubCard)
+  );
+};
+export default React.memo(SubCard);
